0fa01c2b18ea288575616d9127de19bc6e0386f4
francisca <franciscasofia1211@gmail.com>
2026-08-21 10:30:52 -0400

    actualizacion modulo 10
    se creo dos bloques mas con sus respectivos scripts, materiales y demas

---
+++ Assets/Scripts/Bloque 4.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bloque4 : Bloque1
+{
+    void Start()
+    {
+        resistencia = 3;
+    }
+    public override void RebotarBola()
+    {
+        base.RebotarBola();
+    }
+}
---
+++ Assets/Scripts/Bloque 5.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class Bloque5 : Bloque1
+{
+    void Start()
+    {
+        resistencia = 3;
+    }
+    public override void RebotarBola()
+    {
+        base.RebotarBola();
+    }
+}
